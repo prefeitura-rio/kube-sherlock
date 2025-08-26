78ecedb6e9cba6328b068caa7a2009c62840a8dd
Pedro Mendes <pedrohrmendes@proton.me>
2025-08-26 13:09:22 -0300

    fix: change health http server

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from langgraph.store.redis.aio import AsyncRedisStore
 import discord
 from src.agent import create_agent, get_llm_response
 from src.discord import handle_sherlock_message
-from src.healthcheck import run_health_server
+from src.healthcheck import run_http_server
 from src.logger import logger
 from src.mcp import get_mcp_client
 from src.settings import settings
@@ -84,7 +84,7 @@ async def main():
     ):
         bot = SherlockBot(intents, store, checkpointer)
 
-        await asyncio.gather(run_health_server(), bot.start(settings.DISCORD_BOT_TOKEN))
+        await asyncio.gather(run_http_server(), bot.start(settings.DISCORD_BOT_TOKEN))
 
 
 if __name__ == "__main__":
